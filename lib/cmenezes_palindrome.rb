2abfa0c44a86f0da4f5d5e03163523eeda23691b
Chris Menezes <chris@menezes.ca>
2023-01-09 13:56:36 -0500

    Initialize repository

---
+++ lib/cmenezes_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cmenezes_palindrome/version"
+
+module CmenezesPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cmenezes_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CmenezesPalindrome
+  VERSION = "0.1.0"
+end

f10306cec58c77ad61d388645a050de112f9b587
Chris Menezes <chris@menezes.ca>
2023-01-09 14:25:15 -0500

    add some tests and the basic code

--- lib/cmenezes_palindrome.rb
+++ lib/cmenezes_palindrome.rb
@@ -2,7 +2,14 @@
 
 require_relative "cmenezes_palindrome/version"
 
-module CmenezesPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  def processed_content
+    self.to_s.downcase
+  end
 end
